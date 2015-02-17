2ce970064d463e5204652987458a09d21a37727d
nobuki akamatsu <NobukiAkamatsu@Leo.local>
2015-02-17 20:53:36 +0900

    make a change in map structure using 48px image

--- main.js
+++ main.js
@@ -6,10 +6,11 @@ var game;
 var button;
 var kuma;
 var map;
+var mapArray;
 var mapdirect;
-var WIDTH=480;
-var HEIGHT=320;
-var t;
+var WIDTH=960;
+var HEIGHT=960;
+var t=1;
 var field;
 var msglabel;
 var SPRITE_WIDTH  = 320;
@@ -18,7 +19,9 @@ var BACKGROUND_COLOR = 'rgb(185, 130, 190)';
 var WINDOWS_LINE_COLOR = 'rgb(235, 140, 70)';
 var WINDOWS_LINE_COLOR_SHADE = "black";
 var WINDOW_COLOR = 'rgb(251, 215, 157)';
-
+var MAP_NUM = 10;
+var posx;
+var posy;
 /**
  * メッセージ画面クラス
  */
@@ -67,7 +70,7 @@ function preloadAssets(){
    game = new Game(WIDTH,HEIGHT);
    button = new Button("dice","light",50,50);
    kuma = new Sprite(32,32);
-   map = new Map(16, 16);
+   map = new Map(48, 48);
    field = new BaseMessageWindow(300, 70, 10, 220);
    msglabel = new Label();
    msglabel.font = "32px monospace";
@@ -77,7 +80,7 @@ function preloadAssets(){
    msglabel.y = 240;
    game.preload(
 		'images/chara1.png', 
-		'images/map0.png'
+		'489.png'
 		);   
     
     game.rootScene.backgroundColor = "blue";    
@@ -87,96 +90,102 @@ function preloadAssets(){
 }
 
 function init(){
-    
+
     t=1;
     kuma.image = game.assets['images/chara1.png'];
-    map.image = game.assets['images/map0.png'];
-    var file = fs.OpenTextFile("map.txt",1);
+    map.image = game.assets['489.png'];
 
-
-    kuma.x = 8;
-    kuma.y = 8;
-    kuma.i =26;
-    kuma.scaleX = 0.7;
-    kuma.scaleY = 0.7;
-   
+    map_init();
+    // read file functiom 
+    kuma_init();
     game.rootScene.addChild(button);
     game.rootScene.addChild(kuma);
-    var mapArray = [
-		    [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
-		    [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4],
-		    [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4],
-		    [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4],
-		    [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4],
-		    [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4],
-		    [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4],
-		    [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4],
-		    [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4],
-		    [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4],
-		    [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4],
-		    [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4],
-		    [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
-		    ];
-    
-    mapdirect =  [
-		  [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
-		  [4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,4],
-		  [4,0,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,2,4],
-		  [4,0,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,2,4],
-		  [4,0,5,5,5,2,1,1,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,2,4],
-		  [4,0,5,5,5,2,5,0,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,2,4],
-		  [4,0,5,5,5,2,5,0,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,2,4],
-		  [4,0,1,1,1,2,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4],
-		  [4,0,5,5,5,2,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4],
-		  [4,0,5,5,5,2,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4],
-		  [4,0,5,5,5,2,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4],
-		  [4,0,1,1,1,1,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4],
-		  [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
-		  ];
+
     map.loadData(mapArray);
     game.rootScene.addChild(map);
     game.rootScene.addChild(kuma);
 
-    button.moveTo(400,100);
-    t=1;
-    if(t==1){
+    button.moveTo(400,400);
 
-	button.ontouchstart = but;
-	
-    }
+    //    if(t==1){
+    
+    button.ontouchstart = but;
+    
+    // }
     kuma.onenterframe= function(){
-
+	
 	if(t!=1 && 0 < kuma.vx ){
+	    posx = (kuma.x-8)/48;
+	    posy = (kuma.y-8)/48;
+	   
+	    if(mapdirect[posy][posx]==0){
+		kuma.y = kuma.y - 48;
+		kuma.vx--;
+	    }
+	    else if(mapdirect[posy][posx]==1){
+		kuma.x = kuma.x- 48;
+		kuma.vx--;
+	    }
+	    else if(mapdirect[posy][posx]==2){
+		kuma.y = kuma.y+ 48;
+		kuma.vx--;
+	    }
+	    else if(mapdirect[posy][posx]==3){
+		kuma.x = kuma.x+ 48;
+		kuma.vx--;
+	    }
+
 	    
-	    if(mapdirect[(kuma.y+8)/16][(kuma.x+8)/16]==0){
-		kuma.y = kuma.y - 16;
-		kuma.vx--;
-	    }
-	    if(mapdirect[(kuma.y+8)/16][(kuma.x+8)/16]==1){
-		kuma.x = kuma.x- 16;
-		kuma.vx--;
-	    }
-	    if(mapdirect[(kuma.y+8)/16][(kuma.x+8)/16]==2){
-		kuma.y = kuma.y+ 16;
-		kuma.vx--;
-	    }
-	    if(mapdirect[(kuma.y+8)/16][(kuma.x+8)/16]==3){
-		kuma.x = kuma.x+ 16;
-		kuma.vx--;
-	    }
-	    
-	    console.log(kuma.vx);	    
+	   
 	    if(kuma.vx <= 0)
-	    {
-		t=1;
-		game.rootScene.addChild(field);
-		game.rootScene.addChild(msglabel);
-	    }
+		{
+		    t=1;
+		    game.rootScene.addChild(field);
+		    game.rootScene.addChild(msglabel);
+		}
 	}
     }
 }
+function map_init(){
 
+    mapArray = [
+		[4,4,4,4,4,4,4,4,4,4,4,4,4],
+		[4,0,0,0,0,0,0,0,0,0,0,0,4],
+		[4,0,3,3,3,3,3,3,3,3,3,0,4],
+		[4,0,3,3,3,3,3,3,3,3,3,0,4],
+		[4,0,3,3,3,3,3,3,3,3,3,0,4],
+		[4,0,3,3,3,3,3,3,3,3,3,0,4],
+		[4,0,3,3,3,3,3,3,3,3,3,0,4],
+		[4,0,3,3,3,3,3,3,3,3,3,0,4],
+		[4,0,3,3,3,3,3,3,3,3,3,0,4],
+		[4,0,3,3,3,3,3,3,3,3,3,0,4],
+		[4,0,3,3,3,3,3,3,3,3,3,0,4],
+		[4,0,0,0,0,0,0,0,0,0,0,0,4],
+		[4,4,4,4,4,4,4,4,4,4,4,4,4],
+		];
+    mapdirect = [
+		 [4,4,4,4,4,4,4,4,4,4,4,4,4],
+		 [4,3,3,3,3,3,3,3,3,3,3,2,4],
+		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
+		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
+		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
+		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
+		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
+		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
+		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
+		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
+		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
+		 [4,0,1,1,1,1,1,1,1,1,1,1,4],
+		 [4,4,4,4,4,4,4,4,4,4,4,4,4],
+		 ];
+}
 
+function kuma_init(){
+    kuma.x = 48*1+8;
+    kuma.y = 48*1+8;
+    kuma.scaleX = 0.9;
+    kuma.scaleY = 0.8;
+}
 function but()
 {
     game.rootScene.removeChild(field);
@@ -185,7 +194,7 @@ function but()
 	var r = Math.floor(Math.random() * 6) + 1;
 	kuma.vx = r;
 	this.text = "dice:+"+r;
-	console.log("kuma.vx jus = "+kuma.vx);
+	console.log("kuma.vx = "+kuma.vx);
 	t=0;
     }
 }
