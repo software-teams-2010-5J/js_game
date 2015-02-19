b46144ae2766e9bd8023bc9db0a303f18667665f
Akira <sasugasannann@gmail.com>
2015-02-19 17:48:10 +0900

    Update field.js

--- field.js
+++ field.js
@@ -6,111 +6,111 @@ field[0]{
 };
 field[1]{
     name:"地中海通り",
-    value:"60",
-    group_id:"0",
-    effect_id:"0",
+    value:60,
+    group_id:0,
+    effect_id:0,
 };
 field[2]{
     name:"共同基金"
-    value:"0"
-    group_id:"10"
-    effect_id:"3"
+    value:0,
+    group_id:10,
+    effect_id:3,
 };
 field[3]{
     name:"バルティック通り";
-    value:"60";
-    group_id:"0";
-    effect_id:"0";
+    value:60,
+    group_id:0,
+    effect_id:0,
 };
 field[4]{
     name:"所得税";
-    value:"0";
-    group_id:"10";
-    effect_id:"5";
+    value:0,
+    group_id:10,
+    effect_id:5,
 c};
 field[5]{
     name:"リーディング鉄道";
-     value:"200";
-    group_id:"8";
-    effect_id:"1";
+    value:200,
+    group_id:8,
+    effect_id:1,
 };
 field[6]{
     name:"オリエンタル通り";
-    value:"100";
-    group_id:"1";
-    effect_id:"0";
+    value:100,
+    group_id:1,
+    effect_id:0,
 };
 field[7]{
     name:"チャンス";
-    value:"0";
-    group_id:"10";
-    effect_id:"4";
+    value:0,
+    group_id:10,
+    effect_id:4,
 };
 field[8]{
     name:"バーモント通り";
-    value:"100";
-    group_id:"1";
-    effect_id:"0";
+    value:100,
+    group_id:1,
+    effect_id:0,
 };
 field[9]{
     name:"コネチカット通り";
-    value:"120";
-    group_id:"1";
-    effect_id:"0";
+    value:120,
+    group_id:1,
+    effect_id:0,
 };
 field[10]{
     name:"刑務所";
-    value:"50";
-    group_id:"10";
-    effect_id:"7";
+    value:50,
+    group_id:10,
+    effect_id:7,
 };
 field[11]{
     name:"セントチャールズ通り";
-    value:"140";
-    group_id:"2";
-    effect_id:"0";
+    value:140,
+    group_id:2,
+    effect_id:0,
 };
 field[12]{
     name:"電力会社";
-    value:"150";
-    group_id:"9";
-    effect_id:"2";
+    value:150,
+    group_id:9,
+    effect_id:2,
 };
 field[13]{
     name:"ステーツ通り";
-    value:"140";
-    group_id:"2";
-    effect_id:"0";
+    value:140,
+    group_id:2,
+    effect_id:0,
 };
 field[14]{
     name:"バージニア通り";
-    value:"160";
-    group_id:"2";
-    effect_id:"0";
+    value:160,
+    group_id:2,
+    effect_id:0,
 };
 field[15]{
     name:"ペンシルバニア通り";
-    value:"200";
-    group_id:"8";
-    effect_id:"1";
+    value:200,
+    group_id:8,
+    effect_id:1,
 };
 field[16]{
     name:"セントジェームス通り";
-    value:"180";
-    group_id:"3";
-    effect_id:"0";
+    value:180,
+    group_id:3,
+    effect_id:0,
 };
 field[17]{
     name:"共同基金";
-    value:"0";
-    group_id:"10";
-    effect_id:"3";
+    value:0,
+    group_id:10,
+    effect_id:3,
 };
 field[18]{
     name:"テネシー通り";
-    value:"180";
-    group_id:"3";
-    effect_id:"0";
+    value:180,
+    group_id:3,
+    effect_id:0,
 };
 field[19]{
     name: "ニューヨーク通り",
@@ -257,4 +257,4 @@ field[39]{
     value: 400,
     group_id: 7,
     effect_id: 0,
-};
+}
