265cc883b7ca707bb48024b66cacdc085090aa25
Akira Sudou <sasugasannann@gmail.com>
2015-02-20 15:24:42 +0900

    微修正

--- field.js
+++ field.js
@@ -13,103 +13,103 @@ field[1]={
     effect_id:0,
 };
 field[2]={
-    name:"共同基金"
+    name:"共同基金",
     value:0,
     group_id:10,
     effect_id:3,
 };
 field[3]={
-    name:"バルティック通り";
+    name:"バルティック通り",
     value:60,
     group_id:0,
     effect_id:0,
 };
 field[4]={
-    name:"所得税";
+    name:"所得税",
     value:0,
     group_id:10,
     effect_id:5,
-c};
+};
 field[5]={
-    name:"リーディング鉄道";
+    name:"リーディング鉄道",
     value:200,
     group_id:8,
     effect_id:1,
 };
 field[6]={
-    name:"オリエンタル通り";
+    name:"オリエンタル通り",
     value:100,
     group_id:1,
     effect_id:0,
 };
 field[7]={
-    name:"チャンス";
+    name:"チャンス",
     value:0,
     group_id:10,
     effect_id:4,
 };
 field[8]={
-    name:"バーモント通り";
+    name:"バーモント通り",
     value:100,
     group_id:1,
     effect_id:0,
 };
 field[9]={
-    name:"コネチカット通り";
+    name:"コネチカット通り",
     value:120,
     group_id:1,
     effect_id:0,
 };
 field[10]={
-    name:"刑務所";
+    name:"刑務所",
     value:50,
     group_id:10,
     effect_id:7,
 };
 field[11]={
-    name:"セントチャールズ通り";
+    name:"セントチャールズ通り",
     value:140,
     group_id:2,
     effect_id:0,
 };
 field[12]={
-    name:"電力会社";
+    name:"電力会社",
     value:150,
     group_id:9,
     effect_id:2,
 };
 field[13]={
-    name:"ステーツ通り";
+    name:"ステーツ通り",
     value:140,
     group_id:2,
     effect_id:0,
 };
 field[14]={
-    name:"バージニア通り";
+    name:"バージニア通り",
     value:160,
     group_id:2,
     effect_id:0,
 };
 field[15]={
-    name:"ペンシルバニア通り";
+    name:"ペンシルバニア通り",
     value:200,
     group_id:8,
     effect_id:1,
 };
 field[16]={
-    name:"セントジェームス通り";
+    name:"セントジェームス通り",
     value:180,
     group_id:3,
     effect_id:0,
 };
 field[17]={
-    name:"共同基金";
+    name:"共同基金",
     value:0,
     group_id:10,
     effect_id:3,
 };
 field[18]={
-    name:"テネシー通り";
+    name:"テネシー通り",
     value:180,
     group_id:3,
     effect_id:0,
@@ -119,144 +119,124 @@ field[19]={
     value: 200,
     group_id: 3,
     effect_id: 0,
-}:
-
+};
 field[20]={
     name: "駐車場",
     value: 0,
     group_id: 10,
     effect_id: 9,
-}:
-
+};
 field[21]={
     name: "ケンタッキー通り",
     value: 220,
     group_id: 4,
     effect_id: 0,
-}:
-
+};
 field[22]={
     name: "チャンス",
     value: 0,
     group_id: 10,
     effect_id: 9,
-}:
-
+};
 field[23]={
     name: "インディアナ通り",
     value: 220,
     group_id: 4,
     effect_id: 0,
-}:
-
+};
 field[24]={
     name: "イリノイ通り",
     value: 240,
     group_id: 4,
     effect_id: 0,
-}:
-
+};
 field[25]={
     name: "B&O鉄道",
     value: 260,
     group_id: 5,
     effect_id: 0,
-}:
-
+};
 field[26]={
     name: "アトランティック通り",
     value: 260,
     group_id: 5,
     effect_id: 0,
-}:
-
+};
 field[27]={
     name: "ベントノール通り",
     value: 260,
     group_id: 5,
     effect_id: 0,
-}:
-
+};
 field[28]={
     name: "水道会社",
     value:150,
     group_id: 9,
     effect_id: 2,
-}:
-
+};
 field[29]={
     name: "マービンガーデン",
     value: 280,
     group_id: 5,
-    effect_id: 0
-}:
-
+    effect_id: 0,
+};
 field[30]={
     name: "刑務所に入れ",
-    value: 0
-    group_id: 10
-    effect_id: 8
-}:
-
+    value: 0,
+    group_id: 10,
+    effect_id: 8,
+};
 field[31]={
     name: "パシフィック通り",
-    value: 300
-    group_id: 6
-    effect_id: 0
-}:
-
+    value: 300,
+    group_id: 6,
+    effect_id: 0,
+};
 field[32]={
     name: "ノースキャロライナ",
     value: 300,
     group_id: 6,
     effect_id: 0,
-}:
-
+};
 field[33]={
     name: "共同基金",
-    value: 0
-    group_id: 10
-    effect_id: 3
-}:
-
+    value: 0,
+    group_id: 10,
+    effect_id: 3,
+};
 field[34]={
     name: "ペンシルバニア通り",
     value: 320,
     group_id: 6,
     effect_id: 0,
-}:
-
+};
 field[35]={
     name: "ショートライン鉄道",
     value: 200,
     group_id: 8,
     effect_id: 1,
-}:
-
+};
 field[36]={
     name: "チャンス",
     value: 0,
     group_id: 10,
     effect_id: 4,
-}:
-
+};
 field[37]={
     name: "パークプレイス",
     value: 350,
     group_id: 7,
     effect_id: 0,
-}:
-
+};
 field[38]={
     name: "物品税",
     value: 75,
     group_id: 10,
-    effect_id: 6
-}:
-
+    effect_id: 6,
+};
 field[39]={
     name: "ボードウォーク",
     value: 400,
     group_id: 7,
     effect_id: 0,
-}
+};
