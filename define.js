8a79491c80332da9203fab120955b771f2df1362
Akira Sudou <sasugasannann@gmail.com>
2015-02-20 23:54:12 +0900

    マップ初期宣言もdefine.hに含めました。

--- define.js
+++ define.js
@@ -13,3 +13,82 @@ var BACKGROUND_COLOR = 'rgb(185, 130, 190)';
 var WINDOWS_LINE_COLOR = 'rgb(235, 140, 70)';
 var WINDOWS_LINE_COLOR_SHADE = "black";
 var WINDOW_COLOR = 'rgb(251, 215, 157)';
+
+/* メッセージ画面クラス
+ 使い方：use_messageに使いたい文言を追加する。
+ addchildして、表示を消すタイミングでremovechildする。
+ */
+BaseMessageWindow = Class.create(Sprite, {
+    initialize:function(width, height, x, y){
+        // 親クラス コンストラクタ
+        Sprite.call(this, width, height);
+
+        // デフォルト引数
+        if (x === undefined) x=0;
+        if (y === undefined) y=0;
+
+        // サーフェス生成
+	var surface = new Surface(width, height);
+
+        // 描画設定
+        surface.context.beginPath();
+        surface.context.rect(0, 0, width-1, height-1);
+        surface.context.fillStyle = "orange"; // 色指定
+        surface.context.shadowColor = WINDOWS_LINE_COLOR_SHADE;
+	surface.context.shadowBlur = 1;
+        surface.context.shadowOffsetX = 1;
+        surface.context.shadowOffsetY = 1;
+        surface.context.fill();     // 描画
+
+        surface.context.beginPath();
+        surface.context.rect(4, 4, width-7, height-7);
+        surface.context.fillStyle = WINDOW_COLOR; // 色指定
+        surface.context.shadowColor = WINDOWS_LINE_COLOR_SHADE;
+        surface.context.shadowBlur = 1;
+        surface.context.shadowOffsetX = -1;
+        surface.context.shadowOffsetY = -1;
+        surface.context.fill();     // 描画
+
+        // 表示位置設定
+        this.x = x;
+        this.y = y;
+        this.image = surface;
+
+        this.counter = 1;
+    }
+});
+
+//マップ初期宣言
+function map_init(){
+
+    mapArray=[
+	[,,,,,,,,,,,,],
+	[,0,1,2,3,4,5,6,7,8,9,10],
+	[,11,12,13,14,15,16,17,18,19,20,21],
+	[,22,23,24,25,26,27,28,29,30,31,32],
+	[,33,34,35,36,37,38,39,40,41,42,43],
+	[,44,45,46,47,48,49,50,51,52,53,54],
+	[,55,56,57,58,59,60,61,62,63,64,65],
+	[,66,67,68,69,70,71,72,73,74,75,76],
+	[,77,78,79,80,81,82,83,84,85,86,87],
+	[,88,89,90,91,92,93,94,95,96,97,98],
+	[,99,100,101,102,103,104,105,106,107,108,109],
+	[,110,111,112,113,114,115,116,117,118,119,120],
+    ];
+    
+    mapdirect = [
+	[4,4,4,4,4,4,4,4,4,4,4,4,4],
+	[4,3,3,3,3,3,3,3,3,3,3,2,4],
+	[4,0,4,4,4,4,4,4,4,4,4,2,4],
+	[4,0,4,4,4,4,4,4,4,4,4,2,4],
+	[4,0,4,4,4,4,4,4,4,4,4,2,4],
+	[4,0,4,4,4,4,4,4,4,4,4,2,4],
+	[4,0,4,4,4,4,4,4,4,4,4,2,4],
+	[4,0,4,4,4,4,4,4,4,4,4,2,4],
+	[4,0,4,4,4,4,4,4,4,4,4,2,4],
+	[4,0,4,4,4,4,4,4,4,4,4,2,4],
+	[4,0,4,4,4,4,4,4,4,4,4,2,4],
+	[4,0,1,1,1,1,1,1,1,1,1,1,4],
+	[4,4,4,4,4,4,4,4,4,4,4,4,4],
+    ];
+}
--- main.js
+++ main.js
@@ -17,50 +17,6 @@ var SPRITE_WIDTH  = 320;
 var SPRITE_HEIGHT = 280;
 var MAP_NUM = 10;
 var button_t;
-/**
- * メッセージ画面クラス
- 使い方：use_messageに使いたい文言を追加する。
- addchildして、表示を消すタイミングでremovechildする。
- */
-BaseMessageWindow = Class.create(Sprite, {
-    initialize:function(width, height, x, y){
-        // 親クラス コンストラクタ
-        Sprite.call(this, width, height);
-
-        // デフォルト引数
-        if (x === undefined) x=0;
-        if (y === undefined) y=0;
-
-        // サーフェス生成
-	var surface = new Surface(width, height);
-
-        // 描画設定
-        surface.context.beginPath();
-        surface.context.rect(0, 0, width-1, height-1);
-        surface.context.fillStyle = "orange"; // 色指定
-        surface.context.shadowColor = WINDOWS_LINE_COLOR_SHADE;
-	surface.context.shadowBlur = 1;
-        surface.context.shadowOffsetX = 1;
-        surface.context.shadowOffsetY = 1;
-        surface.context.fill();     // 描画
-
-        surface.context.beginPath();
-        surface.context.rect(4, 4, width-7, height-7);
-        surface.context.fillStyle = WINDOW_COLOR; // 色指定
-        surface.context.shadowColor = WINDOWS_LINE_COLOR_SHADE;
-        surface.context.shadowBlur = 1;
-        surface.context.shadowOffsetX = -1;
-        surface.context.shadowOffsetY = -1;
-        surface.context.fill();     // 描画
-
-        // 表示位置設定
-        this.x = x;
-        this.y = y;
-        this.image = surface;
-
-        this.counter = 1;
-    }
-});
 
 function preloadAssets(){
     game = new Game(WIDTH,HEIGHT);
@@ -94,6 +50,8 @@ function preloadAssets(){
     
     var second = new Scene();
     var third = new Scene();
+
+    //テスト文字列
     msglabel[0] = new Label();
     msglabel[0].font = "32px monospace";
     msglabel[0].color = "#000000";
@@ -122,8 +80,9 @@ function preloadAssets(){
     
     
 }
-var i;
+
 function init(){
+    var i;
     for(i = 0;i < 40;i++)
     {
 	console.log(field[i].name);
@@ -158,9 +117,8 @@ function init(){
     kuma.onenterframe= kuma_mov;
     game.pushScene(title);
     button_t.addEventListener("touchend", function(e) { game.popScene(); });
-	
-   
 }
+
 function kuma_mov(){
     
     if(t!=1 && 0 < kuma.vx ){
@@ -198,43 +156,6 @@ function kuma_mov(){
 	    }
     }
 }
-function map_init(){
-    
-
-
-
-    
-    mapArray=[
-	      [,,,,,,,,,,,,],
-	      [,0,1,2,3,4,5,6,7,8,9,10],
-	      [,11,12,13,14,15,16,17,18,19,20,21],
-	      [,22,23,24,25,26,27,28,29,30,31,32],
-	      [,33,34,35,36,37,38,39,40,41,42,43],
-	      [,44,45,46,47,48,49,50,51,52,53,54],
-	      [,55,56,57,58,59,60,61,62,63,64,65],
-	      [,66,67,68,69,70,71,72,73,74,75,76],
-	      [,77,78,79,80,81,82,83,84,85,86,87],
-	      [,88,89,90,91,92,93,94,95,96,97,98],
-	      [,99,100,101,102,103,104,105,106,107,108,109],
-	      [,110,111,112,113,114,115,116,117,118,119,120],
-	      ];
-    
-    mapdirect = [
-		 [4,4,4,4,4,4,4,4,4,4,4,4,4],
-		 [4,3,3,3,3,3,3,3,3,3,3,2,4],
-		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
-		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
-		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
-		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
-		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
-		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
-		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
-		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
-		 [4,0,4,4,4,4,4,4,4,4,4,2,4],
-		 [4,0,1,1,1,1,1,1,1,1,1,1,4],
-		 [4,4,4,4,4,4,4,4,4,4,4,4,4],
-		 ];
-}
 
 function kuma_init(){
     kuma.x = 48*1+8;
@@ -242,6 +163,7 @@ function kuma_init(){
     kuma.scaleX = 0.9;
     kuma.scaleY = 0.8;
 }
+
 function but()
 {
     game.rootScene.removeChild(message_field);
