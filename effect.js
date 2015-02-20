695f176d4eef7bd344b55b6aa82b627a9a24fc8f
Akira Sudou <sasugasannann@gmail.com>
2015-02-21 05:28:29 +0900

    土地関連以外のeffect配列の実装

---
+++ effect.js
@@ -0,0 +1,43 @@
+var effect = [];
+
+effect[0] = {
+    function: function(){
+	console.log("START");
+	}
+};
+effect[1] = {
+    function: function(){
+	console.log("共同基金");
+	}
+};
+effect[2] = {
+    function: function(){
+	console.log("Chance");
+	}
+};
+effect[3] = {
+    function: function(){
+	console.log("所得税");
+	}
+};
+effect[4] = {
+    function: function(){
+	console.log("物品税");
+	}
+};
+effect[5] = {
+    function: function(){
+	console.log("刑務所");
+	}
+};
+effect[6] = {
+    function: function(){
+	console.log("刑務所へ入れ");
+	}
+};
+effect[7] = {
+    function: function(){
+	console.log("駐車場");
+	}
+};
+    
