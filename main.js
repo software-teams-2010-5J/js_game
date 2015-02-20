e8f78fbd5b769fe2b838531c482204b82fd9edd8
Akira Sudou <sasugasannann@gmail.com>
2015-02-20 23:45:11 +0900

    定数的扱いな変数をdefine.jsに集約

--- main.js
+++ main.js
@@ -9,23 +9,13 @@ var kuma;
 var map;
 var mapArray;
 var mapdirect;
-var WIDTH=960;
-var HEIGHT=960;
 var t=1;
 var message_field;
 var msglabel = [];
 var use_message;
 var SPRITE_WIDTH  = 320;
 var SPRITE_HEIGHT = 280;
-var BACKGROUND_COLOR = 'rgb(185, 130, 190)';
-var WINDOWS_LINE_COLOR = 'rgb(235, 140, 70)';
-var WINDOWS_LINE_COLOR_SHADE = "black";
-var WINDOW_COLOR = 'rgb(251, 215, 157)';
 var MAP_NUM = 10;
-var MESSAGE_WINDOW_SIZE_X = 350;
-var MESSAGE_WINDOW_SIZE_Y = 70;
-var MESSAGE_WINDOW_POSITION_X = 10;
-var MESSAGE_WINDOW_POSITION_Y = 220;
 var button_t;
 /**
  * メッセージ画面クラス
